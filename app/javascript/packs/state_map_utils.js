d0705089d62b64a5da780f034bbdb7176b77b36f
Hongyuan Kang <hongyuankang@berkeley.edu>
2023-11-23 00:48:57 +0000

    fixed credentials files

--- app/javascript/packs/state_map_utils.js
+++ app/javascript/packs/state_map_utils.js
@@ -62,9 +62,10 @@ exports.setupEventHandlers = (stateMap) => {
         const countyName = elem.attr('data-county-name');
         return `${countyName}, ${stateMap.state.symbol}`;
     };
+    // changed the url of clickCall
     const clickCallback = (elem) => {
-        const countyFipsCode = elem.attr('data-county-fips-code');
-        window.location.href = `/state/${stateMap.state.symbol}/county/${countyFipsCode}`;
+        const countyName = elem.attr('data-county-name');
+        window.location.href = `/search?address=${countyName}`;
     };
     mapUtils.handleMapMouseEvents(targets, hoverHtmlProvider, clickCallback);
 };
